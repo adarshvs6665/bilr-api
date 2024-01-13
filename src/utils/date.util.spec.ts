d6f84c9cdc3c318d183368767ded45981bdbb1db
Adarsh V S <adarshvs6665@gmail.com>
2024-01-13 13:10:29 +0530

    feat: date util

---
+++ src/utils/date.util.spec.ts
@@ -0,0 +1,6 @@
+describe('DateUtil', () => {
+  it('should be defined', () => {
+    // expect(new ReqLoggerMiddleware()).toBeDefined();
+    expect(1).toBe(1);
+  });
+});
---
+++ src/utils/date.util.ts
@@ -0,0 +1,51 @@
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+dayjs.extend(utc);
+
+export class DateUtil {
+  static today(): Date {
+    return dayjs().utc().toDate();
+  }
+
+  static todayString(): string {
+    return dayjs().utc().format('DD/MM/YYYY HH:mm:ss:SSS');
+  }
+
+  static dateAfter(days: number): Date {
+    return dayjs.utc().add(days, 'day').toDate();
+  }
+
+  static afterMinutes(minutes: number, fromDate?: Date): Date {
+    const baseDate = fromDate || dayjs.utc().toDate();
+    return dayjs(baseDate).add(minutes, 'minute').utc().toDate();
+  }
+
+  static afterHours(hours: number, fromDate?: Date): Date {
+    const baseDate = fromDate || dayjs.utc().toDate();
+    return dayjs(baseDate).add(hours, 'hour').utc().toDate();
+  }
+
+  static afterDays(days: number, fromDate?: Date): Date {
+    const baseDate = fromDate || dayjs.utc().toDate();
+    return dayjs(baseDate).add(days, 'day').utc().toDate();
+  }
+
+  static afterMonths(months: number, fromDate?: Date): Date {
+    const baseDate = fromDate || dayjs.utc().toDate();
+    return dayjs(baseDate).add(months, 'month').utc().toDate();
+  }
+
+  static afterYears(years: number, fromDate?: Date): Date {
+    const baseDate = fromDate || dayjs.utc().toDate();
+    return dayjs(baseDate).add(years, 'year').utc().toDate();
+  }
+
+  static isAfter(date: Date): boolean {
+    return dayjs.utc().isAfter(date);
+  }
+
+  static formatDateString(dateString: string, format: string): string {
+    const parsedDate = dayjs.utc(dateString);
+    return parsedDate.format(format);
+  }
+}
