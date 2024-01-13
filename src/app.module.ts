7662386296ec078f2d1528e4c279a0e0e65c5837
Adarsh V S <adarshvs6665@gmail.com>
2024-01-13 13:09:03 +0530

    feat: request logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BilrLoggerModule } from '@bilr/logger';
+import { ReqLoggerMiddleware } from './middlewares/req-logger/req-logger.middleware';
+import { SERVICE_NAME } from './constants';
 
 @Module({
-  imports: [],
+  imports: [
+    BilrLoggerModule.forRoot({
+      component: SERVICE_NAME,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ReqLoggerMiddleware).forRoutes('*');
+  }
+}
